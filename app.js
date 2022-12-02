b3ea4422d4181ae491bfd07661d17f7a4f55dce5
Patrick Kramer <kramepat@flip3.engr.oregonstate.edu>
2022-12-02 05:55:21 -0800

    add school form works

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 PORT        = 4257;                 // Set a port number at the top so it's easy to change in the future
 // Database
 var db = require('./database/db-connector')
@@ -17,10 +20,63 @@ app.set('view engine', '.hbs');                 // Tell express to use the handl
 /*
     ROUTES
 */
+//Index
 app.get('/', function(req, res)
     {
         res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
     });                                         // will process this file, before sending the finished HTML to the client.
+
+
+//Schools
+app.get('/schools.hbs', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Schools;";                  // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('schools', {data: rows});                // Render the schools.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+
+app.post('/add-school-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Schools (school_name) VALUES ('${data.school_name}')`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Schools;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
 
 /*
     LISTENER
---
+++ public/js/add_school.js
@@ -0,0 +1,76 @@
+// Get the objects we need to modify
+let addSchoolForm = document.getElementById('add-school-form-ajax');
+
+// Modify the objects we need
+addSchoolForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputSchoolName = document.getElementById("input-school_name");
+
+    // Get the values from the form fields
+    let schoolNameValue = inputSchoolName.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        school_name: schoolNameValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-school-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputSchoolName.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Schools
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("schools-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 2 cells
+    let row = document.createElement("TR");
+    let schoolIdCell = document.createElement("TD");
+    let schoolNameCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    schoolIdCell.innerText = newRow.school_id;
+    schoolNameCell.innerText = newRow.school_name;
+
+    // Add the cells to the row 
+    row.appendChild(schoolIdCell);
+    row.appendChild(schoolNameCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
